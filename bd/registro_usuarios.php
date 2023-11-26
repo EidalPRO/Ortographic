cf3262644973bf3a76f4150f06e3a12e0638987c
EidalPRO <eidaloaxaca@gmail.com>
2023-11-25 19:10:21 -0600

    cambios

--- bd/registro_usuarios.php
+++ bd/registro_usuarios.php
@@ -1,36 +1,32 @@
 <?php
-
     include 'conexion_be.php';
-
 
     $ejecutar;
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    //Encriptamiento de contraseña
+    // Encriptamiento de contraseña
     $contrasena = hash('sha512', $contrasena);
 
-    
-    //guardamos datos en la tabla 
+    // Guardamos datos en la tabla 
     $query = "INSERT INTO usuarios(nombre, contrasena) VALUES('$usuario', '$contrasena')";
-    
-    //Verificar que el usuario no se repita en la base de datos
+
+    // Verificar que el usuario no se repita en la base de datos
     $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE nombre='$usuario' ");
+
     
     if(mysqli_num_rows($verificar_usuario) > 0) {
-        //un mensaje de error
-        exit(); // Sale del script PHP
+        echo "existe"; // Usuario ya existe en la base de datos
+        exit(); 
     }
 
     $ejecutar = mysqli_query($conexion, $query);
 
-
     if($ejecutar) {
-        //si se registro con exito
+        echo "exito"; // Registro exitoso
     } else {
-        //en caso de error
+        echo "error"; // Error al registrar usuario
     }
 
     mysqli_close($conexion);
-
 ?>
