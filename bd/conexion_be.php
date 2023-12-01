c46c4cf851d2992dee3ebefd23c2b150fc149d92
EidalPRO <eidaloaxaca@gmail.com>
2023-11-30 22:44:09 -0600

    Variables de entorno

--- bd/conexion_be.php
+++ bd/conexion_be.php
@@ -1,12 +1,17 @@
 <?php
 
-    $conexion = mysqli_connect("localhost", "root", "", "Ortographic_bd");
+    $BD_PORT = "localhost";
+    $BD_USER = "root";
+    $BD_CON = "";
+    $BD_NAME = "Ortographic_bd";
 
-    // // Verificar conexion
-    // if($conexion) {
-    //     echo 'Conectado exitosamente a la base de datos ';
-    // } else {
-    //     echo 'No se ha podido conectar a la base de datos. ';
-    // }
+    $conexion = mysqli_connect($BD_PORT, $BD_USER, $BD_CON, $BD_NAME);
+
+    // Verificar conexion
+    if($conexion) {
+        echo 'Conectado exitosamente a la base de datos ';
+    } else {
+        echo 'No se ha podido conectar a la base de datos. ';
+    }
 
 ?>
