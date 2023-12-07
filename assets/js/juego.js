afca68487e2db7c4ce9e18c76a4a67e61c7b9880
EidalPro <eidaloaxaca@gmail.com>
2023-12-06 18:53:42 -0600

    Cambios en jugar.js

---
+++ assets/js/juego.js
@@ -0,0 +1,275 @@
+document.getElementById("salir").addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+        title: "¿Esta segur@ que desea salir?",
+        text: "Ten en cuenta que se perdera todo tu progreso!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Si, estoy segur@!",
+        cancelButtonText: "No, cancelar!",
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "categoria.php"
+        }
+    });
+
+});
+
+var tema;
+var temaPorsentaje;
+var reactivos = [];
+
+// Función para obtener los datos de la tabla desde PHP
+function obtenerDatos() {
+    // Obtenemos el valor de localStorage
+    var miDato = localStorage.getItem('tema');
+    switch (miDato) {
+        case "t1": tema = "reactivosacento";
+            temaPorsentaje = "tema_1_porcentaje";
+            break;
+        case "t2": tema = "reactivospunto";
+            temaPorsentaje = "tema_2_porcentaje";
+            break;
+        case "t3": tema = "reactivosuso"
+            temaPorsentaje = "tema_3_porcentaje";
+            break;
+        case "t4": tema = "reactivosgramatica";
+            temaPorsentaje = "tema_4_porcentaje";
+            break;
+    }
+
+    // console.log(tema);
+
+    fetch('bd/reactivos.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'dato=' + tema
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            // console.log(data);
+
+            reactivos = data;
+
+            iniciarJuego();
+        })
+        .catch(error => {
+            console.error('Error al obtener datos:', error);
+        });
+}
+window.onload = () => {
+    obtenerDatos();
+};
+
+var preg;
+var res;
+var d1;
+var d2;
+var fed;
+var ora;
+var posCorrect
+var id
+var tiempoInicio;
+var acerto = 0;
+const op1 = document.getElementById("op1");
+const op2 = document.getElementById("op2");
+const op3 = document.getElementById("op3");
+const mostrarReactivo = document.getElementById("mostrar-reactivo");
+
+function iniciarJuego() {
+    tiempoInicio = performance.now();
+    cargarReactivo();
+}
+
+var numerosGenerados = [];
+var preguntasMostradas = 0;
+var posCorrect;
+var tiempoFinal;
+var tiempoTotal;
+
+function cargarReactivo() {
+
+    if (preguntasMostradas === reactivos.length) {
+
+        tiempoFinal = performance.now(); // Guarda el tiempo de finalización
+        tiempoTotal = (tiempoFinal - tiempoInicio) / 1000; // Calcula la diferencia de tiempo
+        // Convierte a segundos: tiempoTotal / 1000
+        porcentajeEfectividad = (acerto * 100) / reactivos.length;
+        // progressBar.setAttribute('aria-valuenow', porcentaje);
+        subirDatos(tiempoTotal, porcentajeEfectividad);
+        Swal.fire({
+            icon: 'info',
+            title: 'Preguntas completadas',
+            text: `Este fue tu porsentaje de efectividad: ${porcentajeEfectividad}% \n
+            \nTu tiempo total en responder las preguntas fue de ${tiempoTotal} segundos.`
+
+        }).then(() => {
+            window.location.href = 'categoria.php';
+        });
+
+    } else {
+
+        do {
+            // console.log(reactivos.length);
+            id = Math.floor(Math.random() * reactivos.length);
+        } while (numerosGenerados.includes(id));
+        numerosGenerados.push(id);
+
+        console.log(id);
+
+        preg = reactivos[id].pregunta;
+        res = reactivos[id].respuesta;
+        d1 = reactivos[id].distractor_1;
+        d2 = reactivos[id].distracor_2;
+        fed = reactivos[id].feedback;
+        ora = reactivos[id].oracionCorrecta;
+
+        var n = Math.floor(Math.random() * 3);
+        posCorrect = (n == 1) ? 'I' : (n == 2) ? 'M' : 'D';
+
+        // console.log(n);
+        console.log(posCorrect);
+
+        mostrarReactivo.innerText = preg;
+        if (posCorrect === 'I') {
+            op1.innerText = res;
+            if (Math.floor(Math.random() * 2) == 1) {
+                op2.innerText = d1;
+                op3.innerText = d2;
+            } else {
+                op2.innerText = d2;
+                op3.innerText = d1;
+            }
+        } else if (posCorrect === 'M') {
+            op2.innerText = res;
+            if (Math.floor(Math.random() * 2) == 1) {
+                op1.innerText = d1;
+                op3.innerText = d2;
+            } else {
+                op1.innerText = d2;
+                op3.innerText = d1;
+            }
+        } else {
+            op3.innerText = res;
+            if (Math.floor(Math.random() * 2) == 1) {
+                op1.innerText = d1;
+                op2.innerText = d2;
+            } else {
+                op1.innerText = d2;
+                op2.innerText = d1;
+            }
+        }
+        preguntasMostradas++;
+    }
+
+    op1.addEventListener("click", function () {
+        if (posCorrect === 'I') {
+            // acerto++;
+            mostrarRespuestaCorrecta();
+        } else {
+            mostrarRespuestaIncorrecta();
+        }
+    });
+
+    op2.addEventListener("click", function () {
+        if (posCorrect === 'M') {
+            // acerto++;
+            mostrarRespuestaCorrecta();
+        } else {
+            mostrarRespuestaIncorrecta();
+        }
+    });
+
+    op3.addEventListener("click", function () {
+        if (posCorrect === 'D') {
+            // acerto++;
+            mostrarRespuestaCorrecta();
+        } else {
+            mostrarRespuestaIncorrecta();
+        }
+    });
+
+}
+
+function mostrarRespuestaCorrecta() {
+    acerto++;
+    Swal.fire({
+        icon: 'success',
+        title: 'Respuesta correcta. ',
+        html: `<p>${ora}</p>` + '<br>' + `<p>${fed}</p>`,
+        confirmButtonText: 'Continuar',
+        padding: '1rem',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: true,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            cargarReactivo();
+        }
+    });
+}
+function mostrarRespuestaIncorrecta() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Respuesta incorrecta. ',
+        html: `<p>${ora}</p>` + '<br>' + `<p>${fed}</p>`,
+        confirmButtonText: 'Continuar',
+        padding: '1rem',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showConfirmButton: true,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            cargarReactivo();
+        }
+    });
+}
+
+function avanzarBarraProgreso() {
+    var porcentaje = (preguntasRespondidas / reactivos.length) * 100;
+    const progressBar = document.querySelector('.progress-bar');
+    progressBar.style.width = porcentaje + '%';
+    progressBar.setAttribute('aria-valuenow', porcentaje);
+}
+
+// Función para subir datos al servidor
+function subirDatos(tiempoTotal, porcentajeEfectividad) {
+    const datos = {
+        tiempoTotal: tiempoTotal,
+        porcentajeEfectividad: porcentajeEfectividad,
+        tema: temaPorsentaje
+    };
+
+    const opciones = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    };
+
+    fetch('bd/subir_datos1.php', opciones)
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                console.log('Datos actualizados correctamente:', data.message);
+            } else {
+                console.error('Error al actualizar datos:', data.message);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
--- juego.php
+++ juego.php
@@ -28,7 +28,7 @@
             <a class="navbar-brand" href="#">
                 <img src="assets/imagenes/logoOrtographic.webp" width="50" alt="Logo Ortographic">
             </a>
-            <p class="nav-item text-center" aria-current="page" href="" id="sala">Sala: </p>
+            <!-- <p class="nav-item text-center" aria-current="page" id="sala_id">Sala: </p> -->
             <a class="nav-item" aria-disabled="true" href="" id="salir">Salir</a>
         </div>
     </nav>
@@ -77,7 +77,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="assets/js/jugar.js"></script>
+    <script src="assets/js/juego.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
