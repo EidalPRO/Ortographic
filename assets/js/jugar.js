160bb2980056b09bdf7f41848af545d91c9c1127
EidalPro <eidaloaxaca@gmail.com>
2023-12-06 20:52:32 -0600

    Jugar ya funcional al 100

--- assets/js/jugar.js
+++ assets/js/jugar.js
@@ -24,23 +24,6 @@ document.getElementById("salir").addEventListener('click', function (event) {
 
 });
 
-window.onload = () => {
-    obtenerCodigoSala();
-}
-const codigoSala = localStorage.getItem('codigoSala');
-function obtenerCodigoSala() {
-    fetch(`bd/obtener_codigo_sala.php?codigo_sala=${codigoSala}`)
-        .then(response => response.text()) // Usar response.text() para obtener el código de sala como texto
-        .then(codigoSala => {
-            const codigoSalaElement = document.getElementById('sala');
-            codigoSalaElement.innerText = "Sala: " + codigoSala;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-
 const op1 = document.getElementById("op1");
 const op2 = document.getElementById("op2");
 const op3 = document.getElementById("op3");
