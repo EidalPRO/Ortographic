994e6ee7caff9374ca5172b2de5f50b611c8d8b7
EidalPro <eidaloaxaca@gmail.com>
2023-12-25 09:34:31 -0600

    Todo bien en index

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 </head>
 
 <body>
-
     <div class="hero">
         <div class="container-fluid text-center img-contenedor">
             <img src="assets/imagenes/hero-orto.webp" class="img-fluid" alt="Ortographic">
