40cffb0c1ce0f1f80f66c01edbbc372cf9571938
EidalPro <eidaloaxaca@gmail.com>
2023-12-30 16:59:27 -0600

    Cambios en la pagina pricipal

--- index.php
+++ index.php
@@ -72,6 +72,14 @@
                     </div>
                     <p>Practicar</p>
                 </div>
+                <!-- <div class="col-6 col-md-4 col-lg-2">
+                    <div class="cir d-flex flex-wrap align-items-center justify-content-center">
+                        <a href="">
+                            <i class="bi bi-joystick"></i>
+                        </a>
+                    </div>
+                    <p>Salas privadas</p>
+                </div> -->
                 <div class="col-6 col-md-4 col-lg-2">
                     <div class="cir d-flex flex-wrap align-items-center justify-content-center">
                         <a href="#galeria">
@@ -103,78 +111,117 @@
     <section id="galeria">
         <div class="container">
             <h1>Galería de fotos.</h1>
-            <div id="carouselExample" class="carousel carousel-dark slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="assets/imagenes/hero-orto.webp" class="d-block w-100 img-fluid" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/imagenes/hero-orto.webp" class="d-block w-100 img-fluid" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/imagenes/hero-orto.webp" class="d-block w-100 img-fluid" alt="...">
+           <div class="container galeria-contenedor ">
+                <div class="icono text-center">
+                    <i class="bi bi-clock-history"></i>
+                </div>
+                <h2 class="text-center">Sección <br> En construcción...</h2>
+                <p class="text-center">Estamos trabajando en ello. </p>
+                <div class="text-center">
+                    <div class="spinner-grow text-warning" role="status">
+                        <span class="visually-hidden"></span>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                    data-bs-slide="prev">
-                    <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
-                    <i class="bi bi-arrow-bar-left"></i>
-                    <span class="visually-hidden">Anterior</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                    data-bs-slide="next">
-                    <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
-                    <span class="visually-hidden">Siguiente</span>
-                    <i class="bi bi-arrow-bar-right"></i>
-                </button>
-            </div>
+           </div>
         </div>
     </section>
 
     <section id="manual">
         <div class="container">
             <h1>Manuales de usuario.</h1>
-            <h2>Sección en construcción.
-                <div class="spinner-grow text-warning" role="status">
-                    <span class="visually-hidden"></span>
+            <div class="container manual-contenedor ">
+                <div class="icono text-center">
+                    <i class="bi bi-clock-history"></i>
                 </div>
-            </h2>
+                <h2 class="text-center">Sección <br> En construcción...</h2>
+                <p class="text-center">Estamos trabajando en ello. </p>
+                <div class="text-center">
+                    <div class="spinner-grow text-warning" role="status">
+                        <span class="visually-hidden"></span>
+                    </div>
+                </div>
+           </div>
         </div>
     </section>
 
     <footer id="creditos">
-        <div class="col-12">
-            <div class="creditos-contenedor text-center">
+        <div class="container">
+            <div class="text-center m-5">
+                <h3>Equipo de trabajo.</h3>
+            </div>
+            <div class="container d-flex footer-contenedor">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card equipo">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-top icono-footer text-center">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Autor 1</h5>
+                                <p class="card-text">Eidal Marquez Ambrosio.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card equipo">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-top icono-footer text-center">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Autor 2</h5>
+                                <p class="card-text">Maia Michelle Morales Ramíres.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card equipo">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-top icono-footer text-center">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Autor 3</h5>
+                                <p class="card-text">Yazmím Maldonado López.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card equipo">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-top icono-footer text-center">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Asesor técnico</h5>
+                                <p class="card-text">Elva Yuridia Morales Luis.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card equipo">
+                            <!-- <img src="..." class="card-img-top" alt="..."> -->
+                            <div class="card-top icono-footer text-center">
+                                <i class="bi bi-person-fill"></i>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Asesor metodológico</h5>
+                                <p class="card-text">Roberto Ruiz Mendoza.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
                 <img src="assets/imagenes/logoOrtographic.webp" alt="Logo Ortographic" class="footer-icono">
-                <div id="contenidoArchivo"></div>
             </div>
         </div>
         <div class="derechos-autor text-center">© 2023 Copyright DGETI - CBTis No. 150.</div>
     </footer>
-
+    
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script>
-        window.addEventListener('load', function () {
-            const contenidoArchivoDiv = document.getElementById('contenidoArchivo');
-            const archivoURL = 'assets/js/creditos.txt';
-            fetch(archivoURL)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('No se pudo cargar el archivo.');
-                    }
-                    return response.text();
-                })
-                .then(data => {
-                    // Reemplaza las comas por saltos de línea en el contenido
-                    data = data.replace(/,/g, '<br>');
-                    contenidoArchivoDiv.innerHTML = data;
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        });
-    </script>
     <script>
         //aqui la alerta y verificcion
         document.getElementById('boton_practicar').addEventListener('click', function (event) {
