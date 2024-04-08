64a1a5179cbcf823872bfb1cc5342916f7acb17b
EidalPro <eidaloaxaca@gmail.com>
2024-04-07 23:06:29 -0600

    agrego opcion de ingresar id de sala

--- selecionar_sala.php
+++ selecionar_sala.php
@@ -40,7 +40,7 @@
                 </div>
                 <div class="col-12 col-md-4">
                     <div class="cir d-flex flex-wrap align-items-center justify-content-center">
-                        <a type="submit" id="boton_privado" href="">
+                        <a type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop" id="boton_privado">
                             <i class="bi bi-joystick"></i>
                         </a>
                     </div>
@@ -48,7 +48,7 @@
                 </div>
                 <div class="col-12 col-md-4">
                     <div class="cir d-flex flex-wrap align-items-center justify-content-center">
-                        <a type="submit" id="boton_privado" href="index.php">
+                        <a type="submit" href="index.php">
                             <i class="bi bi-box-arrow-left"></i>
                         </a>
                     </div>
@@ -59,36 +59,111 @@
 
     </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title fs-5" id="staticBackdropLabel">Salas privadas</h3>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                </div>
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row">
+                            <h5>Disfruta jugando en una sala privada con tus amigos, puedes crear una sala privada o unirte a una utilizando su id.</h5>
+                            <br>
+                            <div class="col-12 col-lg-6">
+                                <div class="cir d-flex flex-wrap align-items-center justify-content-center">
+                                    <a type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" id="ing" href="">
+                                        <i class="bi bi-cloud-plus"></i>
+                                    </a>
+                                </div>
+                                <p>Crear una sala</p>
+                            </div>
+                            <div class="col-12 col-lg-6">
+                                <div class="col-12 col-lg-6">
+                                    <div class="cir d-flex flex-wrap align-items-center justify-content-center">
+                                        <a type="button" data-bs-toggle="modal" data-bs-target="#staticBackdrop3" id="exis" href="">
+                                            <i class="bi bi-cloud-arrow-up"></i>
+                                        </a>
+                                    </div>
+                                    <p>Ingresar a una sala existente</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Regresar</button>
+                    <!-- <button type="button" class="btn btn-primary">Understood</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title fs-5" id="staticBackdropLabel">Sala nueva</h3>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Cancelar</button>
+                    <!-- <button type="button" class="btn btn-primary">Understood</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="staticBackdrop3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title fs-5" id="staticBackdropLabel">Sala existente</h3>
+                    <!-- <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> -->
+                </div>
+                <div class="modal-body">
+                    <h5>Ingresa el id de la sala</h5>
+                    <form action="bd/salas.php" method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <!-- <label for="exampleInputEmail1" class="form-label">Email address</label> -->
+                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="sala">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Aceptar</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Cancelar</button>
+                    <!-- <button type="button" class="btn btn-primary">Understood</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script>
+    <?php
+        if (isset($_GET['error'])) {
+            $error = $_GET['error'];
+            if ($error === "sala_no_encontrada") {
+                echo '
+                    <script>
+                        Swal.fire({
+                            icon: "error",
+                            title: "Oops...",
+                            text: "Parece que la sala no existe, verifica que el id sea correcto y intentalo de nuevo."
+                         });
+                    </script>
+                ';
+            }
+        }
+    ?>
 
-        localStorage.setItem('codigoSala', A0123);
-        // document.getElementById('boton_global').addEventListener('click', function (event) {
-        //     event.preventDefault();
-
-        //     localStorage.setItem('codigoSala', A0123);
-        // });
-
-        //Esto se podra ocupar al implementar las salas privadas
-        // // Obtener el valor del código de sala del formulario
-        // const codigoSala = document.getElementById('salaCod').value.trim();
-        // // Almacenar el código de sala en el Local Storage
-        // localStorage.setItem('codigoSala', codigoSala);
-    </script>
-    <script>
-         document.getElementById('boton_privado').addEventListener('click', function (event) {
-            event.preventDefault();
-
-            Swal.fire({
-                icon: "info",
-                title: "Lo sentimos :(",
-                text: "Por el momento solo contamos con una sala global, estamos trabajando en ello."
-            }); //.then(() => {
-            //     window.location.href = 'inicio_sesion.php';
-            // });
-        });
-    </script>
+    <script src="assets/js/seleccionar_sala.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
         integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
         crossorigin="anonymous"></script>
